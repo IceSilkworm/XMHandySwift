3eda5d485a7c63d77ebc89575dc575bc4c344f5b
ring <renruyun@163.com>
2022-04-14 11:59:34 +0800

    update file 1.0.0

--- XMHandySwift/Classes/Extensions/UIBarButtonItem+.swift
+++ XMHandySwift/Classes/Extensions/UIBarButtonItem+.swift
@@ -15,20 +15,14 @@ public typealias ActionCompletionHandler = () -> Void
 public extension UINavigationItem {
     //left bar
     @discardableResult
-    func leftButtonAction(_ image: UIImage?, action:@escaping ActionCompletionHandler) -> UIButton {
-        let button: UIButton = UIButton(type: .custom)
-        button.setImage(image, for: .normal)
-        button.frame = CGRect(x: 0, y: 0, width: 40, height: 30)
-        button.imageView!.contentMode = .scaleAspectFit;
-        button.contentHorizontalAlignment = .left
-        button.tappedAction(forControlEvents: .touchUpInside, withCallback: {
-            action()
-        })
-        let barButton = UIBarButtonItem(customView: button)
-        let gapItem = UIBarButtonItem(barButtonSystemItem: .fixedSpace, target: nil, action: nil)
-        gapItem.width = -7 //fix the space
-        self.leftBarButtonItems = [gapItem, barButton]
-        return button
+    func leftButtonAction(_ image: UIImage?, _ offSet: CGFloat = 10.0, action:@escaping ActionCompletionHandler) -> UIBarButtonItem {
+        let wrapper = NavClosureWrapper(callback: action)
+        let backBarButtonItem = UIBarButtonItem(image: image?.withRenderingMode(.alwaysOriginal), style: .plain, target: wrapper, action: #selector(NavClosureWrapper.invoke))
+        objc_setAssociatedObject(self, ClosureKeys.PopBackActionKey!, wrapper, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+        
+        backBarButtonItem.imageInsets = UIEdgeInsets(top: 0, left: -offSet, bottom: 0, right: offSet)
+        self.leftBarButtonItem = backBarButtonItem
+        return backBarButtonItem
     }
     
     //right bar
@@ -36,23 +30,23 @@ public extension UINavigationItem {
     func rightButtonAction(_ image: UIImage, action:@escaping (_ button: UIButton) -> Void) -> UIButton {
         let button: UIButton = UIButton(type: .custom)
         button.setImage(image, for: .normal)
-        button.frame = CGRect(x: 0, y: 0, width: 40, height: 30)
-        button.imageView!.contentMode = .scaleAspectFit;
+        button.frame = CGRect(origin: .zero, size: CGSize(width: 30, height: 30))
+        button.imageView?.contentMode = .scaleAspectFit;
         button.contentHorizontalAlignment = .right
         button.tappedAction(forControlEvents: .touchUpInside, withCallback: {
             action(button)
         })
         let barButton = UIBarButtonItem(customView: button)
         let gapItem = UIBarButtonItem(barButtonSystemItem: .fixedSpace, target: nil, action: nil)
-        gapItem.width = -7 //fix the space
-        self.rightBarButtonItems = [barButton, gapItem]
+        gapItem.width = -10 //fix the space
+        self.rightBarButtonItems = [gapItem, barButton]
         return button
     }
 }
 
 private class NavClosureWrapper : NSObject {
     let _callback : () -> Void
-    init(callback : @escaping () -> Void) {
+    init(callback : @escaping ActionCompletionHandler) {
         _callback = callback
     }
     
@@ -61,15 +55,23 @@ private class NavClosureWrapper : NSObject {
     }
 }
 
+
+private extension UINavigationItem {
+    
+    struct ClosureKeys {
+        static let PopBackActionKey = UnsafeRawPointer.init(bitPattern: "PopBackActionKey".hashValue)
+    }
+}
+
 extension UIControl {
     
     private struct ClosureKeys {
-        static let PointerActionKey = UnsafeRawPointer.init(bitPattern: "kPointerActionKey".hashValue)
+        static let TappedActionKey = UnsafeRawPointer.init(bitPattern: "TappedActionKey".hashValue)
     }
     
     fileprivate func tappedAction(forControlEvents events: UIControl.Event, withCallback callback: @escaping () -> Void) {
         let wrapper = NavClosureWrapper(callback: callback)
         addTarget(wrapper, action:#selector(NavClosureWrapper.invoke), for: events)
-        objc_setAssociatedObject(self, ClosureKeys.PointerActionKey!, wrapper, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+        objc_setAssociatedObject(self, ClosureKeys.TappedActionKey!, wrapper, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN_NONATOMIC)
     }
 }
